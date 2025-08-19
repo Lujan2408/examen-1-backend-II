b4a774afcbfd485aaeec8758c9d7b6487d653efc
Jose David <lujan9495@gmail.com>
2025-08-18 21:51:56 -0500

    added entity scan for letting spring boot scan packages

--- src/main/java/com/example/Examen1Back2/Examen1Back2Application.java
+++ src/main/java/com/example/Examen1Back2/Examen1Back2Application.java
@@ -2,8 +2,10 @@ package com.example.Examen1Back2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.example.Examen1Back2.modelos")
 public class Examen1Back2Application {
 
 	public static void main(String[] args) {
